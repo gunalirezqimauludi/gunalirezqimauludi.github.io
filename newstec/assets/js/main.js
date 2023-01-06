2ddf0062ed8d062856645863cf3915164fab399e
Gunali Rezqi Mauludi <hello.gunalirezqi@gmail.com>
2023-01-06 09:36:32 +0700

    Add Newstec

---
+++ newstec/assets/js/main.js
@@ -0,0 +1,118 @@
+let news = [{
+        'id': '1',
+        'date': '30/03/2020',
+        'image': 'https://blue.kumparan.com/image/upload/bgbaqrxmxthooxw0rf8t.jpg',
+        'title': 'Mudik Bawa Petaka Corona',
+        'author': 'Kumparan',
+        'authorImage': 'https://1.bp.blogspot.com/-LJVVJngpT9o/XZ1HEVevXBI/AAAAAAAAALM/dyFo1mZYk3IjLgjF1YiXaH3eIfEdVtUjgCLcBGAsYHQ/s1600/logo%2Bu%2Bkumparan%2Bghasais.png',
+        'contentImage': 'https://blue.kumparan.com/image/upload/vnp1cypmhusue45zzue0.jpg',
+        'contentNews': 'Hari masih pagi di Jembatan Gantung, Cengkareng, Jakarta Barat, Minggu (22/3). Rusmanto berdiri di depan loket salah satu agen bus di deretan pertokoan pinggir jalan itu.<br><br> Selembar tiket bus tujuan Baturetno, Wonogiri, Jawa Tengah, terselip di kantongnya. Jauh sebelum Lebaran tiba, ia memutuskan pulang kampung lebih cepat. <br><br> Penyebaran virus severe acute respiratory syndrome coronavirus 2 (Sars-CoV-2) yang menjadi biang penyakit COVID-19, memaksa pabrik tempatnya bekerja diliburkan. Manajemen tak bisa memberi kepastian kapan pabrik akan kembali beroperasi. <br><br> Nasib Rusmanto yang mengandalkan penghidupan dari honor harian sebagai sopir jadi tak menentu. Daripada tak ada pemasukan di ibu kota, dia memilih pulang kampung. <br><br> Hari semakin siang. Suasana di loket agen bus tempat Rusmanto menunggu pun bertambah ramai. <br><br> “Tahu-tahu bus penuh, jadi pada pulang dadakan. Alasannya buruh proyek diliburkan serentak. Kebanyakan mereka bekerja di proyek, dan proyeknya berhenti,” cerita Rusmanto kepada kumparan, Sabtu (28/3).<br><br> Penyebaran COVID-19 di Jakarta membuat banyak orang asal daerah memutuskan pulang kampung. Sigit, pekerja reparasi pendingin ruangan, adalah contoh lainnya. <br><br> Omzet usahanya anjlok 70 persen sejak wabah COVID-19 meluas. Dalam kondisi normal, pria yang sudah merintis bisnis sejak 2010 ini bisa dipanggil 2-3 tempat pelanggan per hari. Dalam sebulan, Sigit bisa mengantongi Rp 8 juta. <br><br> Namun kini, di tengah wabah corona yang melanda ibu kota, dia harus melepas banyak orderan. <br><br> “Di Jakarta itu zona merahnya semakin meluas. Jadi ya daripada kami risiko (terinfeksi), mending tidak diambil (ordernya),” ujarnya. <br><br> Penurunan omzet membuat pengeluaran Sigit lebih besar pasak daripada tiang. Setiap bulan, ia harus membayar sewa kontrakan. <br><br> Belum lagi, urusan biaya hidup sehari-hari yang tidak bisa ditawar-tawar. Sigit dan keluarganya terpaksa berhemat sana-sini demi bertahan hidup di Jakarta.<br><br>'
+    },
+    {
+        'id': '2',
+        'date': '27/03/2020',
+        'image': 'https://blue.kumparan.com/image/upload/zennagoblkzrpxsd029a.jpg',
+        'title': 'Virus Corona di Indonesia: Rata-rata 3 Pasien Meninggal Setiap Hari',
+        'author': 'Kumparan',
+        'authorImage': 'https://1.bp.blogspot.com/-LJVVJngpT9o/XZ1HEVevXBI/AAAAAAAAALM/dyFo1mZYk3IjLgjF1YiXaH3eIfEdVtUjgCLcBGAsYHQ/s1600/logo%2Bu%2Bkumparan%2Bghasais.png',
+        'contentImage': 'https://blue.kumparan.com/image/upload/axkeib7fldvqht8kvxsx.jpg',
+        'contentNews': 'Kasus pasien positif virus corona di Indonesia melonjak tinggi. Data yang disampaikan juru bicara pemerintah untuk penanganan corona, Achmad Yurianto, per Jumat (27/3), menyatakan pasien positif virus corona telah mencapai angka 1.046. <br><br> Tak hanya itu, angka kematian karena virus corona juga turut bertambah. Di tanggal yang sama, terdapat penambahan 9 kasus kematian. Dengan begitu, total sudah ada 87 kematian akibat virus corona di Indonesia. <br><br> Sedangkan, 46 pasien dinyatakan sembuh, atau bertambah 11 dari hari sebelumnya.<br><br> Terkait kasus kematian, di Indonesia baru terjadi di hari ke-10 setelah diumumkannya kasus pertama dan kedua positif corona. Namun, sejak pengumuman kasus kematian pertama pada 11 Maret lalu, angka kematian terus bertambah. <br><br> kumparan mencoba menghitung rata-rata kematian yang terjadi di Indonesia akibat virus corona. Data yang kami gunakan berasal dari worldometers.info, sebuah situs yang menampilkan garis waktu penularan virus corona di seluruh dunia secara real time. <br><br> Adapun rumus untuk penghitungannya adalah sebagai berikut: <br><br> Jumlah kematian ter-update dibagi jumlah hari sejak kasus positif corona pertama diumumkan pada 2 Maret 2020. <br><br> Dalam hal ini, 87 kasus kematian dibagi 26 hari. Hasilnya memunculkan angka 3,34 yang kami bulatkan menjadi 3. <br><br> Berdasarkan perhitungan itu, dapat disebutkan bahwa terdapat rata-rata 3 kasus kematian akibat corona di Indonesia setiap harinya. <br><br>'
+    },
+    {
+        'id': '3',
+        'date': '26/03/2020',
+        'image': 'https://blue.kumparan.com/image/upload/ylzulnhtn34es8fivymd.jpg',
+        'title': 'Mengukur Kesiapan Indonesia Menghadapi Puncak Wabah Corona',
+        'author': 'Kumparan',
+        'authorImage': 'https://1.bp.blogspot.com/-LJVVJngpT9o/XZ1HEVevXBI/AAAAAAAAALM/dyFo1mZYk3IjLgjF1YiXaH3eIfEdVtUjgCLcBGAsYHQ/s1600/logo%2Bu%2Bkumparan%2Bghasais.png',
+        'contentImage': 'https://blue.kumparan.com/image/upload/kkpbegv7agjejrctzexs.jpg',
+        'contentNews': 'Kasus positif virus corona yang bertambah setiap harinya di Indonesia semakin dikhawatirkan sejumlah pihak. Para peneliti kesehatan dalam dan luar negeri turut menyoroti kesiapan Indonesia menangani wabah yang ditetapkan PBB sebagai pandemi ini.<br><br> Prakiraan ambruknya fasilitas kesehatan (faskes) pun mencuat kala pemerintah terus menekankan upaya kuratif (penyembuhan) ketimbang antisipatif. Salah satunya dari Peneliti epidemiologi klinis FKUI-RSCM, dokter Tifauzia Tyassuma.<br><br> “Membangun rumah sakit darurat, membeli APD (alat pelindung diri), membeli obat yang belum ketahuan efektivitasnya, itu upaya kuratif. Sementara peningkatan kesadaran masyarakat atas COVID-19 sangat terbatas. Kalau begitu terus, faskes bisa ambruk pada kasus ke-1.000. Saya perkirakan paling lambat 10 hari dari sekarang,” kata Tifauzia kepada kumparan (25/3).<br><br> Hingga Kamis (26/3), pemerintah melaporkan kasus positif virus corona di Indonesia mencapai 893 orang. Menurut estimasi studi Center for Mathematical Modelling of Infectious Diseases yang berbasis di London, Inggris, jumlah itu hanya 2 persen dari total kasus yang berhasil dikonfirmasi. Sementara itu, peneliti ITB memprediksi kasus corona di Indonesia akan mencapai puncaknya pada pertengahan April dan baru berakhir pada awal Juni 2020. Di akhir periode wabah, diperkirakan ada 60 ribu kasus positif COVID-19 dari total 269,6 juta jiwa penduduk RI.<br><br> Bagaimana kesiapan RI untuk menghadapi masa-masa sulit tersebut? Salah satu cara mengukurnya adalah dengan melihat rasio jumlah fasilitas dan sumber daya kesehatan yang tersedia di suatu negara. Untuk fasilitas kesehatan, Organisasi Kerja Sama dan Pembangunan Ekonomi (OECD), World Bank, hingga WHO kerap menilik data rasio jumlah ranjang di rumah sakit per 1.000 penduduk.<br><br> "Indikator ini memberikan ukuran sumber daya yang tersedia untuk memberikan layanan kepada pasien rawat inap di rumah sakit dalam hal jumlah tempat tidur yang dirawat, dikelola, dan segera tersedia untuk digunakan," tulis OECD dalam situs resminya.'
+    },
+    {
+        'id': '4',
+        'date': '26/03/2020',
+        'image': 'https://blue.kumparan.com/image/upload/diowrnxnrjudkwuzk5qq.jpg',
+        'title': 'Herd Immunity Bukan Jawaban untuk Virus Corona',
+        'author': 'Kumparan',
+        'authorImage': 'https://1.bp.blogspot.com/-LJVVJngpT9o/XZ1HEVevXBI/AAAAAAAAALM/dyFo1mZYk3IjLgjF1YiXaH3eIfEdVtUjgCLcBGAsYHQ/s1600/logo%2Bu%2Bkumparan%2Bghasais.png',
+        'contentImage': 'https://blue.kumparan.com/image/upload/oha3lrvkjb663kvcfgja.jpg',
+        'contentNews': ' Januari lalu, yang terasa sudah seperti selamanya, Menteri Kesehatan Republik Indonesia Terawan Agus Putranto menjargon agar masyarakat Indonesia tak perlu resah pada virus corona.<br><br> Katanya, Senin (27/1) lalu, “Dari 1,4 miliar penduduk sana (China), paling dua ribuan (yang meninggal akibat corona), itu kan kayak apa. Karena itu, pencegahannya jangan panik, jangan resah.” Terawan bilang, “Enjoy saja, makan yang cukup.” <br><br> Pertengahan Februari, menanggapi pertanyaan soal harga masker yang mahal, ia sewot, “Salahmu sendiri, kok beli ya. Enggak usah (pakai). Masker untuk yang sakit.” <br><br> Pertengahan Maret, usai menjenguk tiga orang yang sembuh dari COVID-19, Terawan juga bilang, “Saya bahagia teorinya benar, bahwa ini self-limited disease, penyakit yang akan sembuh sendiri.” <br><br> Senin (22/3), saat mendampingi Menhan Prabowo Subianto dalam penerimaan simbolis alat kesehatan dari China, wajah Terawan tertutup masker dan tangannya dibungkus sarung tangan medis. <br><br> Rabu (25/3) kemarin, 23 hari setelah kasus positif pertama corona diumumkan, 790 orang dinyatakan positif COVID-19. Sebanyak 58 di antaranya meninggal, sedang yang sembuh cuma 31 orang. <br><br> Sampai menjelang akhir Maret ini, tak ada kebijakan lockdown di Indonesia. Tak ada pula karantina wilayah, yang opsinya sebetulnya sudah tersedia melalui UU Nomor 6 Tahun 2018 tentang Kekarantinaan Kesehatan. <br><br> Seorang pejabat pemerintah sempat berujar di forum tertutup, andai kasus infeksi corona di Indonesia tinggi sekalipun, itu tak apa. “Yang penting imunnya kuat. Kalau kuat, tak akan fatal. Tidak perlu lockdown segala. Biarkan masyarakat beraktivitas seperti biasa.” <br><br> Pemerintah percaya bahwa kebijakan lockdown tidak efektif. Yang kemudian pemerintah lakukan adalah menganjurkan physical distancing, meningkatkan kapabilitas fasilitas kesehatan (di Pulau Galang dan Wisma Atlet Kemayoran), memulai rapid test massal, membeli stok avigan dan klorokuin, serta contact tracing yang hasilnya tak dibuka ke umum. <br><br> Bagi dr. Tifauzia Tyassuma, Direktur Eksekutif Center for Clinical Epidemiology & Evidence-Based Medicine FKUI-RSCM, pemerintah salah strategi dalam menghadapi virus corona. Pemerintah dinilai tergesa mengambil langkah-langkah kuratif namun lamban dan terlambat dalam menjalankan langkah preventif. <br><br> Melihat langkah yang diambil dan keengganan luar biasa untuk melakukan lockdown, Tifa berpandangan pemerintah memilih untuk mengejar herd immunity—sebuah langkah yang menurutnya sangat spekulatif dan berbahaya. <br><br> Ia menilai, apabila herd immunity memang menjadi pilihan pemerintah, korban tewas akibat COVID-19 akan mencapai jumlah yang sangat besar. Utamanya adalah mereka dari kelompok usia rentan dan yang memiliki komorbid.<br><br>'
+    },
+    {
+        'id': '5',
+        'date': '26/03/2020',
+        'image': 'https://blue.kumparan.com/image/upload/tqrirf8mdwiu2wxsgnlb.jpg',
+        'title': 'Corona di RI Menyebar ke 1 Provinsi per Hari, Lebih Parah dari Italia dan Korsel',
+        'author': 'Kumparan',
+        'authorImage': 'https://1.bp.blogspot.com/-LJVVJngpT9o/XZ1HEVevXBI/AAAAAAAAALM/dyFo1mZYk3IjLgjF1YiXaH3eIfEdVtUjgCLcBGAsYHQ/s1600/logo%2Bu%2Bkumparan%2Bghasais.png',
+        'contentImage': 'https://blue.kumparan.com/image/upload/klz1ozuhgpffxlxbpexu.jpg',
+        'contentNews': 'Angka kasus positif virus corona di Indonesia terus melonjak tiap hari. Sejak kasus 1 dan 2 yang diumumkan tanggal 2 Maret 2020 hingga laporan teranyar pada 25 Maret, sudah ada 790 kasus yang dilaporkan. <br><br> Di antara kasus-kasus tersebut, 31 pasien dinyatakan sembuh dan diperbolehkan pulang dari rumah sakit. Sementara, 58 pasien lainnya meninggal.<br><br> Tak hanya jumlah kasus yang melonjak, penyebaran wilayah kasus positif corona juga kian meluas. Awalnya, penyebaran corona di Indonesia dimulai dari seorang warga Depok (kasus 1) yang kontak dengan WN Jepang positif corona di Restoran Amigos, Jakarta Selatan. <br><br> Warga Depok tersebut lalu kontak dengan ibunya di rumahnya. Keduanya merupakan kasus 1 dan 2 yang mengawali persebaran kasus di dua provinsi, yakni Jawa Barat (Depok) dan DKI Jakarta.<br><br> Seiring waktu, kasus corona berkembang di berbagai wilayah di Indonesia. Laporan terbaru dari Kementerian Kesehatan RI, pada 25 Maret terdapat 790 kasus positif di 24 provinsi. <br><br> Tanggal tersebut merupakan hari ke-24 penularan virus corona di Indonesia sejak pertama kali diumumkan. Apabila dirata-ratakan, maka dalam 1 hari terdapat 1 provinsi baru yang wilayahnya terdapat kasus positif corona.<br><br>'
+    },
+    {
+        'id': '6',
+        'date': '25/03/2020',
+        'image': 'https://blue.kumparan.com/image/upload/rxnmswkokvzbneuriqxz.jpg',
+        'title': 'WFH: Sekadar Imbauan Tak Bertaji',
+        'author': 'Kumparan',
+        'authorImage': 'https://1.bp.blogspot.com/-LJVVJngpT9o/XZ1HEVevXBI/AAAAAAAAALM/dyFo1mZYk3IjLgjF1YiXaH3eIfEdVtUjgCLcBGAsYHQ/s1600/logo%2Bu%2Bkumparan%2Bghasais.png',
+        'contentImage': 'https://blue.kumparan.com/image/upload/dmdwev8ml5pedv5yp2rz.jpg',
+        'contentNews': 'Askolani, bukan nama sebenarnya, cuma bisa pasrah. Karyawan PT Permodalan Nasional Madani—salah satu anak perusahaan BUMN—ini harus berdesakan di kereta listrik menuju kantor di tengah gentingnya penyebaran wabah COVID-19, Senin (23/3). <br><br> Ia tak punya pilihan. PT PNM tetap mewajibkan sebagian karyawannya masuk kantor. Kebijakan itu kontras dengan arahan Presiden Jokowi yang berulang kali meminta masyarakat menjaga jarak sosial, salah satunya dengan bekerja dari rumah (work from home). <br><br> Asko dan rekan-rekannya tak mendapat jawaban pasti kenapa perusahaan masih mengharuskan karyawan bekerja di kantor. <br><br> “Kalau kata direksi, ‘Dirut sendiri belum ada instruksi (WFH) dari Kementerian BUMN karena Menteri BUMN Pak Erick Thohir masih masuk,’” kata Asko menirukan ucapan manajemen kantornya. <br><br> kumparan yang mencoba mengonfirmasi hal tersebut kepada Kementerian BUMN, belum mendapat jawaban. <br><br> Sementara itu, kolega Asko yang menolak masuk kantor di tengah merebaknya wabah COVID-19, memilih meliburkan diri. Mereka pun kehilangan uang makan dan transpor harian. <br><br> Ketidaksesuaian antara imbauan pemerintah dengan regulasi perusahaan BUMN membuat pekerja seperti Asko berada di posisi sulit. <br><br> “BUMN dan pemerintah saja belum sinkron, sedangkan kondisinya sudah begini ngeri,” katanya. <br><br> Bagi Asko, masalah makin pelik. Kantor tempatnya bekerja tidak punya alat penunjang sanitasi yang memadai. <br><br> “Masker juga enggak ada, hand sanitizer aja kurang,” ujar Asko. Ia tambah cemas. <br><br> Belum lagi, ia harus pergi-pulang menggunakan moda KRL Commuterline. Tak heran Asko tak pernah tenang bekerja dua hari belakangan. <br><br> Lagi-lagi, dia cuma bisa pasrah. “Berjuang aja, bismillah, meski ngeri juga saya.” <br><br> Ketika kumparan mengkonfirmasi soal aturan masuk kantor di tengah wabah corona ke PT PNM, perwakilan mereka menolak berkomentar. <br><br> “Mohon maaf, untuk kali ini PT PNM tidak bersedia untuk di-interview terkait kebijakan WFH dikarenakan satu dan lain hal,” ujarnya, meminta namanya tak disebut.'
+    }
+];
+
+renderNews();
+
+const newsContent = document.querySelectorAll(".news-card");
+for (let news of newsContent) {
+    news.addEventListener('click', function (event) {
+        const id = event.target.closest('article').getAttribute("data-id");
+        viewDetailNews(id);
+
+        const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        if (width <= 1183) {
+            document.querySelector(".content").style.display = 'none';
+            document.querySelector("aside").style.display = 'block';
+        }
+    });
+}
+
+const navigation = document.querySelectorAll("nav>ul>li");
+for (let nav of navigation) {
+    nav.addEventListener('click', function (event) {
+        const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        if (width <= 1183) {
+            document.querySelector(".content").style.display = 'block';
+            document.querySelector("aside").style.display = 'none';
+        }
+    });
+}
+
+function renderNews() {
+    let newsContent = document.querySelector(".news-content");
+    newsContent.innerHTML = "";
+
+    let newsList = "";
+    news.map(data => {
+        newsList += `<article class="news-card" data-id="${data.id}">
+                    <img src="${data.image}" class="news-card-image" alt="${data.title}">
+                    <div class="news-card-info">
+                        <p class="news-card-info-time">${data.date}</p>
+                        <h2 class="news-card-info-title">${data.title}</h2>
+                        <p class="news-card-info-author">${data.author}</p>
+                    </div>
+                </article>`;
+    });
+    newsContent.innerHTML = newsList;
+}
+
+function viewDetailNews(id) {
+    let filter = news.find(data => data.id == id);
+
+    let header = document.querySelector(".news-detail-header");
+    let title = document.querySelector(".news-detail-title");
+    let content = document.querySelector(".news-detail-content");
+
+    header.innerHTML = `<img src="${filter.authorImage}" alt="${filter.author}"><p> By <span class="text-hightlight">${filter.author}</span> ${filter.date}</p>`;
+    title.innerHTML = filter.title;
+    content.innerHTML = `<img src="${filter.contentImage}" class="news-detail-content-image" alt="${filter.title}">${filter.contentNews}`;
+}
